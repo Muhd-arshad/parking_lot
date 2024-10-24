b4ca14c83383ed8ac9661ca27a13caa61e68be1a
Muhammed-Arshad-n <148439118+Muhammed-Arshad-n@users.noreply.github.com>
2024-10-24 16:34:13 +0530

    changed the border clr

--- lib/view/history_screen.dart
+++ lib/view/history_screen.dart
@@ -34,7 +34,7 @@ class HistoryScreen extends StatelessWidget {
                         margin: const EdgeInsets.all(8),
                         padding: const EdgeInsets.all(12),
                         decoration: BoxDecoration(
-                          border: Border.all(color: Colors.blue),
+                          border: Border.all(color: Colors.black),
                           borderRadius: BorderRadius.circular(10),
                         ),
                         child: Column(
